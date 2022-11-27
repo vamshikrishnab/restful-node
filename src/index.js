b3864cfdd6c416dbcd35ed92ccdd8675dae5fc79
Vamshikrishna B <86870@global.ul.com>
2022-11-28 00:07:30 +0530

    rest arch with a smaple app

---
+++ src/index.js
@@ -0,0 +1,72 @@
+// importing the dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const {startDatabase} = require('./database/mongo');
+const {insertAd, getAds} = require('./database/ads');
+const {deleteAd, updateAd} = require('./database/ads');
+
+// defining the Express app
+const app = express();
+
+// defining an array to work as the database (mock)
+const ads = [
+  {title: 'Hello, world!'}
+];
+
+// adding Helmet to enhance your API's security
+app.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+
+// enabling CORS for all requests
+app.use(cors());
+
+// adding morgan to log HTTP requests
+app.use(morgan('combined'));
+
+
+
+app.get('/', async (req, res) => {
+    res.send(await getAds());
+  });
+
+
+// starting the server
+app.listen(3001, () => {
+  console.log('listening on port 3001');
+});
+
+
+
+app.post('/', async (req, res) => {
+    const newAd = req.body;
+    await insertAd(newAd);
+    res.send({ message: 'New ad inserted.' });
+  });
+  
+  // endpoint to delete an ad
+  app.delete('/:id', async (req, res) => {
+    await deleteAd(req.params.id);
+    res.send({ message: 'Ad removed.' });
+  });
+  
+  // endpoint to update an ad
+  app.put('/:id', async (req, res) => {
+    const updatedAd = req.body;
+    await updateAd(req.params.id, updatedAd);
+    res.send({ message: 'Ad updated.' });
+  });
+
+
+  startDatabase().then(async () => {
+    await insertAd({title: 'Hello, from the in-memory database!'});
+  
+    // start the server
+    app.listen(3001, async () => {
+      console.log('listening on port 3001');
+    });
+  });
